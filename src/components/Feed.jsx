86eace098c5f126b3cb66fc736fd166a68d998b9
haresZee <ebrahem.haress@gmail.com>
2023-06-23 00:17:47 +0300

    Checking

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,9 +8,9 @@ const Feed = () => {
   
   useEffect (()=>{
     FetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>setVideos(data.items))
+    console.log(videos);
 
   },[selectedCategory])
-  console.log(videos);
   return (
     <Stack sx={{ flexDirection:{sx:"column", md:"row"}}}>
       <Box sx={{height: {sx:'auto', md:'92vh'},borderRight:'1px solid #3d3d3d',px:{sx:0, md:2}}}>
