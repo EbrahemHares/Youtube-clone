e84647110609f6904e627388a46f8ad2d91424c7
haresZee <ebrahem.haress@gmail.com>
2023-06-24 22:19:50 +0300

    see

--- src/setupProxy.jsx
+++ src/setupProxy.jsx
@@ -1,23 +1,23 @@
-//This file will contain the configuration for the http-proxy-middleware.
-import dotenv from 'dotenv';
+// //This file will contain the configuration for the http-proxy-middleware.
+// import dotenv from 'dotenv';
 
-dotenv.config();
-import { createProxyMiddleware } from 'http-proxy-middleware';
-const rapidApiKey =  process.env.VITE_RAPID_API_KEY;
+// dotenv.config();
+// import { createProxyMiddleware } from 'http-proxy-middleware';
+// const rapidApiKey =  process.env.VITE_RAPID_API_KEY;
 
 
-const proxyy = 
-        createProxyMiddleware({
-            target: 'https://youtube-v31.p.rapidapi.com',
-            changeOrigin: true,
-            pathRewrite: {
-            '^/api': '/',
-            },
-            headers: {
-                'X-RapidAPI-Key': rapidApiKey,
-                'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-            }
-        });
-    ;
+// const proxyy = 
+//         createProxyMiddleware({
+//             target: 'https://youtube-v31.p.rapidapi.com',
+//             changeOrigin: true,
+//             pathRewrite: {
+//             '^/api': '/',
+//             },
+//             headers: {
+//                 'X-RapidAPI-Key': rapidApiKey,
+//                 'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+//             }
+//         });
+//     ;
 
-export {proxyy}
+// export {proxyy}
