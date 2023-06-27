9fe7fde7579217921eff0bd31a5840971e1cd96e
haresZee <ebrahem.haress@gmail.com>
2023-06-27 14:17:42 +0300

    changing configurations for deployment to netlify

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from "./components
 
 function App() {
   return (
-    <BrowserRouter basename='/Youtube-clone'>
+    <BrowserRouter >
       <Box sx={{backgroundColor:'#000'}}>
         <Navbar />
         <Routes>
